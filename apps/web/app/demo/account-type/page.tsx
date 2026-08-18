467019cf189f9d26c2720944d4d1b5b7a36e535e
rodmesh-lgtm <rod.mesh@gmail.com>
2026-08-18 16:32:30 +0300

    Remove legacy account-type demo route

--- apps/web/app/demo/account-type/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { AccountTypeSelectionPrototype } from "../../../components/organization-prototype";
-
-export default function DemoAccountTypePage() {
-  return <AccountTypeSelectionPrototype onSelect={() => undefined} selected={null} />;
-}
